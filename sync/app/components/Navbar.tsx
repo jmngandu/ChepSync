fa113c606d73de50560e10a14006379642a3432f
jmngandu <jamesmngandu@gmail.com>
2024-11-02 16:58:55 +0300

    sync

--- sync/app/components/Navbar.tsx
+++ sync/app/components/Navbar.tsx
@@ -2,8 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { auth, signOut, signIn } from "@/auth";
+
 const Navbar = async () => {
   const session = await auth();
+
+  const handleLogin = async () => {
+    "use server";
+    await signIn({ provider: "github" });
+  };
+
   return (
     <>
       <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
@@ -12,7 +19,7 @@ const Navbar = async () => {
             <Image src="/logo.jpg" alt="logo" width={70} height={30} />
           </Link>
           <div className="flex items-center gap-5 text-black">
-            {session && session?.user ? (
+            {session && session.user ? (
               <>
                 <Link href="./startup/create">
                   <span>create</span>
@@ -20,16 +27,14 @@ const Navbar = async () => {
                 <button onClick={signOut}>
                   <span>Logout</span>
                 </button>
-                <Link href={`/user/${session?.id}`}>
-                  <span>{session?.user?.name}</span>
+                <Link href={`/user/${session.id}`}>
+                  <span>{session.user.name}</span>
                 </Link>
               </>
             ) : (
-                <button onClick={async()=>{
-                    "use server";
-                      await signIn(provider:'github')} }>
-                    <span>Login</span>
-                </button>
+              <button onClick={handleLogin}>
+                <span>Login</span>
+              </button>
             )}
           </div>
         </nav>
@@ -37,4 +42,5 @@ const Navbar = async () => {
     </>
   );
 };
+
 export default Navbar;
