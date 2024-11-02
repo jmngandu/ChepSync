9df89da57c04cac3d42c2fa4c4c938d79bb0ded0
jmngandu <jamesmngandu@gmail.com>
2024-11-02 12:42:47 +0300

    sync

--- sync/app/components/Navbar.tsx
+++ sync/app/components/Navbar.tsx
@@ -1,9 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
-
-const Navbar = () => {
+import { auth, signOut, signIn } from "@/auth";
+const Navbar = async () => {
+  const session = await auth();
   return (
     <>
-      <div className="px-5 py-3 bg-white shadow-sm font-work-sans">NAVBAR</div>
+      <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+        <nav className="flex justify-between items-center">
+          <Link href="/">
+            <Image src="/logo.jpg" alt="logo" width={70} height={30} />
+          </Link>
+          <div className="flex items-center gap-5 text-black">
+            {session && session?.user ? (
+              <>
+                <Link href="./startup/create">
+                  <span>create</span>
+                </Link>
+                <button onClick={signOut}>
+                  <span>Logout</span>
+                </button>
+                <Link href={`/user/${session?.id}`}>
+                  <span>{session?.user?.name}</span>
+                </Link>
+              </>
+            ) : (
+                <button onClick={async()=>{
+                    "use server";
+                      await signIn(provider:'github')} }>
+                    <span>Login</span>
+                </button>
+            )}
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
