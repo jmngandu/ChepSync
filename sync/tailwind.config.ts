037fccb1641da72a93b7ca97e67431bc7d56c6dd
jmngandu <jamesmngandu@gmail.com>
2024-11-03 10:19:35 +0300

    chep sync

--- sync/tailwind.config.ts
+++ sync/tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
   darkMode: ["class"],
@@ -45,7 +47,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [tailwindAnimate, typography],
 };
 
 export default config;
